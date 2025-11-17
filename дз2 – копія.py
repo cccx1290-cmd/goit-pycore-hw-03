1f57bef8f0149702f024c7b9f3590c12a31c4185
cccx1290-cmd <cccx12.90@gmail.com>
2025-11-17 18:12:28 +0200

    Add files via upload

---
+++ дз2 – копія.py
@@ -0,0 +1,18 @@
+import random
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
+
+    if (
+        min < 1 or
+        max > 1000 or
+        quantity <= 0 or
+        quantity > (max - min + 1) or
+        min > max
+    ):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+
+    return sorted(numbers)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
